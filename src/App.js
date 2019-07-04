4f8ca7fb5572f4ec349071632dfd29ff5eba4338
carloscoto <cmcoto@gmail.com>
2019-07-04 23:30:31 +0200

    worked on css module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Recipe from './Recipe';
 import './App.css';
 
+
 const App = () => {
 
 const APP_ID = "2d014b95";
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './myRecipe.module.css';
 
 
 const Recipe = ({title,calories,image, ingredients}) => {
     return(
-        <div>
-            <h1>{title}</h1>
+        <div >
+            <h1 className={styles.recipe}>{title}</h1>
             <p>{calories} Calories</p>
-            <img src={image} alt={title}/>
+            <img className={styles.image} src={image} alt={title}/>
             <ol>
                 {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
+                    <li key={Math.random()}>{ingredient.text}</li>
                 ))}
             </ol>
         </div>
