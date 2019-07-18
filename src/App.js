a6db7f79fabe219016312d9150d4b8f8d2e9ad4c
carloscoto <cmcoto@gmail.com>
2019-07-18 12:45:34 +0200

    Work on Recipe

--- src/App.js
+++ src/App.js
@@ -1,63 +1,68 @@
-import React, {useEffect, useState} from 'react';
-import Recipe from './Recipe';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
 const App = () => {
+  const APP_ID = "2d014b95";
+  //process.env.REACT_APP_API_ID;
+  const APP_KEY = "374bf4b05d9a82c0666c6a3dff2505c8";
+  //process.env.REACT_APP_API_KEY;
 
-const APP_ID = "2d014b95";
-//process.env.REACT_APP_API_ID;
-const APP_KEY = "374bf4b05d9a82c0666c6a3dff2505c8";
-//process.env.REACT_APP_API_KEY;
+  //console.log(process.env.REACT_APP_API_ID)
+  //console.log(process.env.REACT_APP_API_KEY)
 
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
-//console.log(process.env.REACT_APP_API_ID)
-//console.log(process.env.REACT_APP_API_KEY)
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
 
-const [recipes, setRecipes] = useState([]);
-const [search, setSearch] = useState('');
-const [query, setQuery] = useState('chicken');
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
 
-useEffect( () => {
-  getRecipes();
-}, [query]);
-
-const getRecipes = async () => {
-  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-  const data = await response.json();
-  setRecipes(data.hits);
-}
-
-const updateSearch = e => {
-  setSearch(e.target.value);
-}
-
-const getSearch = e => {
-  e.preventDefault();
-  setQuery(search);
-  setSearch('');
-}
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
 
   return (
-<div className="App">
-    <form onSubmit={getSearch} className="search-form">
-      <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-      <button className="search-button" type="submit">Search</button>
-    </form>
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
 
-    <div className="recipes">
-    {recipes.map(recipe => (
-      <Recipe 
-        key={recipe.recipe.label}
-        title={recipe.recipe.label}
-        calories={Math.round(recipe.recipe.calories)}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-      />
-    ))}
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={Math.round(recipe.recipe.calories)}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
-</div>
   );
 };
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import styles from './myRecipe.module.css';
+import React from "react";
+import styles from "./myRecipe.module.css";
 
 
-const Recipe = ({title,calories,image, ingredients}) => {
-    return(
-        <div >
-            <h1 className={styles.recipe}>{title}</h1>
-            <p>{calories} Calories</p>
-            <img className={styles.image} src={image} alt={title}/>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li key={Math.random()}>{ingredient.text}</li>
-                ))}
-            </ol>
-        </div>
-    );
-}
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={styles.recipe}>
+   
 
-export default Recipe
+      <h1>{title}</h1>
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <p>{calories} Calories</p>
+      
+      <img className={styles.image} src={image} alt={title} />
+      
+      
+
+      <ol>
+        {ingredients.map(ingredient => (
+          <li key={Math.random()}>{ingredient.text}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default Recipe;
